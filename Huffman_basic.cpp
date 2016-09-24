a6a77e8f3b802fd547c3bbb58794114156cfc6fe
Chris <chris.g14@iiits.in>
2016-09-24 17:23:12 +0530

    Added comments to Huffman basic

--- Huffman_basic.cpp
+++ Huffman_basic.cpp
@@ -1,3 +1,13 @@
+/*
+Author : Chris Andrew
+Email : chris.g14@iiits.in
+This code was written for educational purposes and is free to use and modify by anyone.
+
+Usage:
+The compiled file must be called on the command line using ./<compiled_file> <input_file> <output_file>
+Reads the text from <input_file> and writes the Huffman tree to <output_file>
+*/
+
 #include <iostream>
 #include <queue>
 #include <string>
@@ -7,6 +17,8 @@
 #include <fstream>
 #include <stdio.h>
 using namespace std;
+
+//An object that represents a charcter or a symbol in the <input_file>
 struct symb
 {
 	string a;
@@ -14,6 +26,8 @@ struct symb
 	symb* l;
 	symb* r;
 };
+
+//Structure to compare the values of two 'symb' objects; Will be given to sorting function for sorting.
 struct comp
 {
 	bool operator()(symb * a, symb * b) const
@@ -24,30 +38,33 @@ struct comp
 	}
 };
 
+//Huffman class constructs the Huffman tree, stores it in a dictionary and prints it to an output file
 class Huffman
 {
 private:
-	map<char,int> chctrs;//frequency of all characters in infile
-	map<char,string> bin;//binary string for representing each character
-	string infile;//name of input file
-	string outfile;//name of output file
-	int k[1000000];//k has contents of total file
-	int num;
-	symb *root;//root node of Huffman tree
+	map<char,int> chctrs;			//frequency of all characters in infile
+	map<char,string> bin;			//binary string for representing each character
+	string infile;			//name of input file
+	string outfile;			//name of output file
+	int k[1000000];			//k has contents of total file, limit set to 1000000 characters
+	int num;			//stores the number of characters
+	symb *root;			//root node of Huffman tree
 public:
-	
+
+	//Class constructor
 	Huffman(string a, string b)
 	{
 		infile=a;
 		outfile=b;
 	}
 
-	void filein()//reads contents of input file and calculates frequency of each character (into "chctrs")
+	//Reads contents of input file and calculates frequency of each character
+	void filein()
 	{
 		char temp[100000];
 		int x;
 		for(x=0;x<infile.size();x++)
-			temp[x]=infile[x];
+		temp[x]=infile[x];
 		temp[x]='\0';
 		num=0;
 		ifstream f;
@@ -66,7 +83,9 @@ public:
 		f.close();
 		return;
 	}
-	void constructtree()//constructs Huffman tree(into "root") and finds binary string of each character(into "bin")
+
+	//Constructs Huffman tree and finds binary string of each character
+	void constructtree()
 	{
 		priority_queue<symb*,vector<symb*>,comp> q;
 		symb *t1,*t2,*t3;
@@ -84,7 +103,6 @@ public:
 		}
 		while(q.size()>1)
 		{
-			
 			t1=(q.top());
 			q.pop();
 			t2=(q.top());
@@ -95,20 +113,20 @@ public:
 			t3->l=t1;
 			t3->r=t2;
 			q.push(t3);
-			
 		}
 		char temp[100000];
 		int x;
 		for(x=0;x<outfile.size();x++)
+		{
 			temp[x]=outfile[x];
+		}
 		temp[x]='\0';
 		ofstream f;
 		f.open(temp);
-		for(it=chctrs.begin(); it!=chctrs.end(); ++it)//Constructing map "bin" and storing it in "decode.txt"
+		for(it=chctrs.begin(); it!=chctrs.end(); ++it)
 		{
 			t1=q.top();
 			bin[it->first]="";
-			
 			while(t1->l!=NULL&&t1->r!=NULL)
 			{
 				if((t1->l)->a.find(it->first)<(t1->l)->a.size())
@@ -121,16 +139,15 @@ public:
 					bin[it->first]=bin[it->first]+"1";
 					t1=t1->r;
 				}
-				
 			}
 			f<<bin[it->first]<<" ";
-			
+			cout<<bin[it->first]<<endl;
 		}
-		
+		f.close();
 		return;
 	}
-	
 };
+
 int main(int argc,char *argv[])
 {
 	string a(argv[1]);
@@ -138,6 +155,6 @@ int main(int argc,char *argv[])
 	Huffman h(a,b);
 	h.filein();
 	h.constructtree();
-	
+
 	return 0;
 }
